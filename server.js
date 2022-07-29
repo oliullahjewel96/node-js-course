c9400415df535a0026ad3eaf95534ee889f33bc4
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-07-29 15:30:07 +0600

    Sending html page to browser

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const http = require("http");
-
+const fs = require("fs");
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     //set header content type
     res.setHeader("Content-Type", "text/html");
-    res.write("<h1>Hey I am a Web developer </h1>");
-    res.write("<h5> My name is oli ullah jewel </h5>");
-    res.end();
+    // res.write("<h1>Hey I am a Web developer </h1>");
+    // res.write("<h5> My name is oli ullah jewel </h5>");
+
+    fs.readFile("views/index.html", (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
 });
 
 server.listen(3000, "localhost", () => {
